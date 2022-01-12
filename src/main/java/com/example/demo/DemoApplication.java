2dbca0179eea1f62c9bae2c61e249fecce1e8231
len <58143415+LenHoChi@users.noreply.github.com>
2022-01-13 00:53:09 +0700

    sen

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("this is main class edit 1 one more 123 edit 123");
-		System.out.println("123 1234");
+		System.out.println("123 1234 12345");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
