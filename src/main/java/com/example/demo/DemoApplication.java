9b1d4ff9bfb43a477c03e9634b11f2ca6f832bfd
len <58143415+LenHoChi@users.noreply.github.com>
2022-01-13 00:24:59 +0700

    add one more

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("this is main class edit 1");
+		System.out.println("this is main class edit 1 one more");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
