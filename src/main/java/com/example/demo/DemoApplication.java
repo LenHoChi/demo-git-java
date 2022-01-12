409e9de8879d46da8432c154f5b71b06cd0115f6
len <58143415+LenHoChi@users.noreply.github.com>
2022-01-12 23:35:52 +0700

    branch 2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("in ra cai gì vậy edit");
 		System.out.println("in ra cai gì vậy");
+		System.out.println("in ra cai gì vậy branch 2");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
