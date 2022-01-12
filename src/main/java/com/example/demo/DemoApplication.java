6d4370ad1d3ca62009bc0cf253b9cdd57a732b2d
LenHoChi <58143415+LenHoChi@users.noreply.github.com>
2022-01-13 01:17:30 +0700

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("this is main class edit 1 one more 123 edit 123");
 		System.out.println("123 1234 12345 11 12");
-		System.out.println("add one line");
+		System.out.println("add one line 2");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
