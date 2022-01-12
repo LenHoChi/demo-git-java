5ea2d5786362e0acdf6ebc98601275572a6fae95
len <58143415+LenHoChi@users.noreply.github.com>
2022-01-13 01:26:30 +0700

    edit line 3 confict

--- src/main/java/com/example/demo/len.java
+++ src/main/java/com/example/demo/len.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class len {
     public static void main(String[] args) {
         System.out.println("this is len class");
+        System.out.println("123");
     }
 }
