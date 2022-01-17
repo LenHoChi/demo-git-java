597ed8205247b9974e3ac188a0c1d8ecb3ef6f46
len <58143415+LenHoChi@users.noreply.github.com>
2022-01-17 16:35:15 +0700

    commit c3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("commit c1");
 		System.out.println("commit c2");
+		System.out.println("commit c3");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
