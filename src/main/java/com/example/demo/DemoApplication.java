a9dcdd0b0dbadb4108380293a8685de761f47f3f
LenHoChi <58143415+LenHoChi@users.noreply.github.com>
2022-01-17 16:33:42 +0700

    Update DemoApplication.java
    commit c3 confilct

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		System.out.println("commit c1");
 		System.out.println("commit c2");
+		System.out.println("commit c3 conflict");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
