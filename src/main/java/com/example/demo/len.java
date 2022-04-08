6aa3292c270b3d0956db58599dbd937d597ed0e6
LenHoChi <58143415+LenHoChi@users.noreply.github.com>
2022-04-08 13:43:34 +0700

    commit 123

--- src/main/java/com/example/demo/len.java
+++ src/main/java/com/example/demo/len.java
@@ -4,5 +4,6 @@ public class len {
     public static void main(String[] args) {
         System.out.println("this is len class");
         System.out.println("123");
+        System.out.println("123");
     }
 }
